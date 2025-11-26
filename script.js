624cae457f03797965f3e8cd1537d8240efae0c5
Andrey Paes <andreivison1997@gmail.com>
2025-11-25 22:06:33 -0400

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Links
+document.getElementById("btnWeb").onclick = () =>
+  (window.location.href = "https://seuappweb.com")
+
+document.getElementById("btnApk").onclick = () =>
+  (window.location.href = "/downloads/ptemp.apk")
+
+// Parallax das imagens laterais
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX - window.innerWidth / 2) / 40
+
+  document.querySelector(
+    ".left-img"
+  ).style.transform = `translateY(-50%) translateX(${x}px)`
+
+  document.querySelector(
+    ".right-img"
+  ).style.transform = `translateY(-50%) translateX(${x * -1}px)`
+})
+
+// Partículas de luz
+const particles = document.getElementById("particles")
+
+for (let i = 0; i < 35; i++) {
+  const star = document.createElement("div")
+  star.className = "particle"
+
+  star.style.left = Math.random() * 100 + "vw"
+  star.style.top = Math.random() * 100 + "vh"
+  star.style.animationDuration = 4 + Math.random() * 6 + "s"
+
+  particles.appendChild(star)
+}
